9197eb60e10de1714b4e423348fc6b14904b937a
Alexander Jenke <alexander.jenke@tu-dresden.de>
2019-04-24 09:55:41 +0200

    Initial Commit

---
+++ MBPSingleStroke/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MBPSingleStroke
+//
+//  Created by Alexander Jenke on 24.04.19.
+//  Copyright © 2019 Alexander Jenke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
